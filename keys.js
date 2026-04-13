02917a633185af680540f0d00c4f913214f564fd
lutfi <lutfullaustaz@gmail.com>
2026-04-13 14:08:21 -0700

    Create keys.js

---
+++ keys.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyD0ooLSXpiG0AR_9x-AJPENqiNfVuDaxzQ",
+    authDomain: "fridge-377e6.firebaseapp.com",
+    projectId: "fridge-377e6",
+    storageBucket: "fridge-377e6.firebasestorage.app",
+    messagingSenderId: "790150025357",
+    appId: "1:790150025357:web:d316ed8c273952649e92c4"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
